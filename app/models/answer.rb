a2eff94a12c850e7f82d001af195dc25d2725356
Adam LaFave <lafave.adam@gmail.com>
2014-07-14 14:38:11 -0700

    add validations for Answer, Question models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,7 @@
 class Answer < ActiveRecord::Base
   # Relationships
   belongs_to :question
+
+  # Validations
+  validates :question, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ActiveRecord::Base
   # Relationships
   belongs_to :quiz
   has_many   :answers
+
+  # Validations
+  validates :quiz, presence: true
 end
