2fdb74a6f770ba41d94297397d40c4a2e8c66c81
Adam LaFave <lafave.adam@gmail.com>
2014-07-14 14:45:23 -0700

    add/run AddAnswersCountToQuestions migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714214326) do
+ActiveRecord::Schema.define(version: 20140714214450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140714214326) do
     t.integer  "quiz_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "answers_count", default: 0
   end
 
   create_table "quizzes", force: true do |t|
