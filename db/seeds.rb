97905f55a2cff5fafc62ccebe94021f206a3d2c6
Adam LaFave <lafave.adam@gmail.com>
2014-07-14 15:17:54 -0700

    update seeds.rb to import data from quizzes.json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Import quizzes
+quizzes_json = JSON.parse(open("#{Dir.pwd}/db/quizzes.json").read)
+
+quizzes_json["quizzes"].each do |quiz|
+  new_quiz = Quiz.create
+
+  quiz.each do |question, answers|
+    new_question = new_quiz.questions.create body: question
+
+    answers.each_with_index do |answer, i|
+      new_question.answers.create correct: i.zero?, url: answer
+    end
+  end
+end
