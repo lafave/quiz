a3b91a9cf3500c532e16aabed51650dfda2ce115
Adam LaFave <lafave.adam@gmail.com>
2014-07-14 17:59:59 -0700

    fix quiz <-> attempts relationship

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,6 +1,6 @@
 class Attempt < ActiveRecord::Base
   # Relationships
   belongs_to :user,      counter_cache: true
-  has_one    :quiz
+  belongs_to :quiz
   has_many   :responses, dependent: :destroy
 end
