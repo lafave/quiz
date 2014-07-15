a8495559c75cd98d996b1e49ef9fc8ba59a68975
Adam LaFave <lafave.adam@gmail.com>
2014-07-14 18:49:25 -0700

    add Attempt#score, update show page for attempts

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -3,4 +3,9 @@ class Attempt < ActiveRecord::Base
   belongs_to :user,      counter_cache: true
   belongs_to :quiz
   has_many   :responses, dependent: :destroy
+
+  def score
+    correct_answers = quiz.answers.where(correct: true).map(&:id) & responses.map(&:answer_id)
+    (correct_answers.size.to_f / quiz.questions_count) * 100
+  end
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,5 @@ class Quiz < ActiveRecord::Base
   # Relationships
   has_many :attempts,  dependent: :destroy
   has_many :questions, dependent: :destroy
+  has_many :answers,   through: :questions
 end
