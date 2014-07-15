b855908953d4ab793ffa99a8a57c5cec0bb779c9
Adam LaFave <lafave.adam@gmail.com>
2014-07-15 16:48:06 -0700

    add/run AddQuestionIdIndexToAnswers migration

---
+++ db/migrate/20140715234657_add_question_id_index_to_answers.rb
@@ -0,0 +1,5 @@
+class AddQuestionIdIndexToAnswers < ActiveRecord::Migration
+  def change
+    add_index :answers, :question_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715003406) do
+ActiveRecord::Schema.define(version: 20140715234657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20140715003406) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "attempts", force: true do |t|
     t.integer  "user_id"
