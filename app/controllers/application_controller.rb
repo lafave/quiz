78dfbbb68cd5539d6a89d2da0f021da3aa2cb1c4
Adam LaFave <lafave.adam@gmail.com>
2014-07-15 12:44:10 -0700

    handle ActiveRecord::RecordNotFound errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  private
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false
+  end
 end
