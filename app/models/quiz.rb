b4149840fab2aa964160db79d78e29503a34be1f
Adam LaFave <lafave.adam@gmail.com>
2014-07-14 17:05:37 -0700

    add relationships to Response, User, Quiz models

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,5 @@
 class Quiz < ActiveRecord::Base
   # Relationships
+  has_many :attempts,  dependent: :destroy
   has_many :questions, dependent: :destroy
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,2 +1,5 @@
 class Response < ActiveRecord::Base
+  # Relationships
+  belongs_to :attempt
+  has_one    :answer, inverse_of: false
 end
