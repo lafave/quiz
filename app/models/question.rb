7eb9dfd23f0830933aee028314adb508caed33b4
Adam LaFave <lafave.adam@gmail.com>
2014-07-15 12:58:00 -0700

    ensure each question only has one correct answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,10 +4,21 @@ class Question < ActiveRecord::Base
   has_many   :answers, dependent: :destroy
 
   # Validations
+  validate  :only_one_correct_answer
   validates :answers, length: { :maximum => 4 }
   validates :quiz,    presence: true
 
+  # @return [Answer, nil]
   def correct_answer
     answers.where(correct: true).first
+  end
+
+  private
+
+  # @return [void]
+  def only_one_correct_answer
+    if answers.correct.size > 0
+      errors.add :answers, "A question can only have one correct answer"
+    end
   end
 end
