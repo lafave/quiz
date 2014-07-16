ab16c434f7c908c317a5b933c40300f5ab8d57c0
Adam LaFave <lafave.adam@gmail.com>
2014-07-15 17:23:58 -0700

    add Question model spec

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,8 +3,17 @@ class Answer < ActiveRecord::Base
   belongs_to :question, counter_cache: true
 
   # Validations
+  validate  :only_correct_answer
   validates :question, presence: true
 
   # Scopes
   scope :correct, -> { where correct: true }
+
+  private
+
+  def only_correct_answer
+    if question.answers.correct.size > 0
+      errors.add :question, "A correct answer already exists for this question"
+    end
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,20 +4,11 @@ class Question < ActiveRecord::Base
   has_many   :answers, dependent: :destroy
 
   # Validations
-  validate  :only_one_correct_answer
-  validates :quiz,    presence: true
+  validates :quiz, presence: true
+  validates_associated :answers
 
   # @return [Answer, nil]
   def correct_answer
     answers.where(correct: true).first
-  end
-
-  private
-
-  # @return [void]
-  def only_one_correct_answer
-    if answers.correct.size > 0
-      errors.add :answers, "A question can only have one correct answer"
-    end
   end
 end
